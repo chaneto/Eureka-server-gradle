baf9cb8bb6288612c2036ad082ce019713cd7fa1
Stoyan Stoyanov <chaneto800ss@gmail.com>
2022-05-25 14:50:32 +0300

    Initial commit

---
+++ src/test/java/com/example/Eurekaservergradle/EurekaServerGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Eurekaservergradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
